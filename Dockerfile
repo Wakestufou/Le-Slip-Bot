da910c488721ced15f2f4a1eb5f502476e6221a8
Wakestufou <>
2025-04-26 19:40:39 +0200

    Add deploy commands + README

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM node:23-bookworm
+
+# Timezone
+ENV TZ=Europe/Paris
+
+# Dépendances système pour build natif
+RUN apt-get update && apt-get install -y \
+  build-essential \
+  python3 \
+  pkg-config \
+  libc6-dev \
+  && rm -rf /var/lib/apt/lists/*
+
+# Créer un dossier de travail
+WORKDIR /slip-bot
+
+# Copier uniquement les fichiers nécessaires au début pour profiter du cache Docker
+COPY package*.json ./
+
+# Installer les dépendances
+RUN npm ci
+
+# Copier le reste du projet
+COPY . .
+
+# Construire l'app TypeScript
+RUN npm run build
+
+# Vérification de la timezone
+RUN echo "Timezone: $TZ" && date
+
+# Lancer l'app
+CMD ["npm", "start"]
---
+++ src/deploy-commands.ts
@@ -0,0 +1,67 @@
+import fs from 'fs';
+import path from 'path';
+import 'dotenv/config';
+import { REST, Routes } from 'discord.js';
+
+const isDev = process.argv.slice(2)[0] === 'DEV';
+const isDeleteDevCommands = process.argv.slice(2)[0] === 'DELETE';
+
+if (!process.env.TOKEN || !process.env.CLIENT_ID) {
+    console.log(`[FATAL] Please complete .env file!`);
+}
+
+// @ts-ignore
+const commands = [];
+
+if (!isDeleteDevCommands) {
+    const foldersPath = path.join(__dirname, 'commands');
+    const commandFolder = fs.readdirSync(foldersPath);
+
+    for (const folder of commandFolder) {
+        const commandsPath = path.join(foldersPath, folder);
+        const commandFiles = fs
+            .readdirSync(commandsPath)
+            .filter((file) => file.endsWith('.ts') || file.endsWith('.js'));
+
+        for (const file of commandFiles) {
+            const filePath = path.join(commandsPath, file);
+            const command = require(filePath).default;
+
+            if ('data' in command && 'execute' in command) {
+                commands.push(command.data.toJSON());
+            } else {
+                console.log(
+                    `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+                );
+            }
+        }
+    }
+}
+
+// @ts-ignore
+const rest = new REST().setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        if (isDev || isDeleteDevCommands) {
+            await rest.put(
+                // @ts-ignore
+                Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+                {
+                    // @ts-ignore
+                    body: commands,
+                }
+            );
+        } else {
+            await rest.put(
+                // @ts-ignore
+                Routes.applicationCommands(process.env.CLIENT_ID),
+                { body: commands }
+            );
+        }
+    } catch (error) {
+        console.error(error);
+    }
+})();
